a5761551fcbfc569435767630318d4c839096db1
Chandra TERA <Chandrawpsiloto@gmail.com>
2023-10-11 08:52:25 +0700

    Menghitung penghasilan kerja
    Membuat operator sederhana dengan menghitung nilai dari waktu, kerja dan hasil

---
+++ TUGAS 2-2.php
@@ -0,0 +1,31 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>TUGAS 2-2</title>
+ </head>
+ <body>
+ 
+      <?php
+
+            $waktu = 36;
+                $kerja = 12;
+                $t_kerja = 24;
+                $hasil;
+                echo ("36 hari = 12 Pekerja");
+                print "<br>";
+                echo ("X hari = 24 Pekerja");
+                print "<br>";
+                echo ("X =... ?");
+                $hasil = $waktu*$kerja/$t_kerja;
+                print "<br>";
+                echo ("Jawaban : $hasil");
+
+      ?>
+
+
+ </body>
+ </html>
+ 
+ 
